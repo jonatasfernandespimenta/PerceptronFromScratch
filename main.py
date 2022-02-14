a6ed52c847fe08a0beefcc63ec7cb4577986683d
Jônatas Fernandes Pimenta <51496759+jonatasfernandespimenta@users.noreply.github.com>
2022-02-14 16:20:20 -0300

    fix some errors | uses heaviside from numpy
    todo: fix rmse function
        return np.sqrt(np.mean((y - y_hat) ** 2))
    ValueError: operands could not be broadcast together with shapes (150,) (2,2)

--- main.py
+++ main.py
@@ -6,40 +6,16 @@ from sklearn.linear_model import Perceptron
 dataset = load_iris()
 
 # petal length and width
-x = np.array(dataset.data[:, (2, 3)]).astype(np.int64)
+x = dataset.data[:, (2, 3)]
 # Iris Setosa?
 y = (dataset.target == 0).astype(np.int64)
 
-weights = np.array(np.random.rand(2, 150)).astype(np.int64)
+weights = np.random.rand(150, 2)
 
 class Perceptron:
   def __init__(self, weights, bias=1):
     self.weights = weights
     self.bias = bias
-  
-  def heaviside(self, z):
-    for i in range(len(z)):
-      if z[0][i] < 0:
-        return -1
-      if z[0][i] == 0:
-        return 0
-      if z[0][i] > 0:
-        return 1
-
-  def sgn(self, z):
-    for i in range(len(z)):
-      if z[0][i] < 0:
-        return -1
-      if z[0][i] == 0:
-        return 0
-      if z[0][i] > 0:
-        return 1
-
-  def activation(self, func, z):
-    if func == 'heaviside':
-      return self.heaviside(z)
-    if func == 'sgn':
-      return self.sgn(z)
 
   def rmse(self, y, y_hat):
     return np.sqrt(np.mean((y - y_hat) ** 2))
@@ -47,36 +23,40 @@ class Perceptron:
   def update(self, x, y, y_hat, n, error):
     for i in range(len(x)):
       for j in range(len(y)):
-        result = self.weights[i][j] + np.sum(np.dot((n * (y[j] - y_hat)), x[i]))
-        self.weights[i][j] += result
+        result = self.weights[i][0] + np.sum(np.dot((n * (y[j] - y_hat)), x[i]))
+        self.weights[i][0] += result
+        result = self.weights[i][1] + np.sum(np.dot((n * (y[j] - y_hat)), x[i]))
+        self.weights[i][1] += result
 
-  def fit(self, x, y, activation, n, epochs=1):
-    for i in range(epochs):
-      u = np.dot(self.weights, x)
-      y_hat = self.activation(activation, u)
-      
+  def fit(self, x, y, n, epochs=1):
+    loop = True
+    while loop == True:
+      u = np.dot(np.transpose(self.weights), x)
+      y_hat = np.heaviside(u, 0.5)
+      print(y_hat)
+
       error = self.rmse(y, y_hat)
+
       if error >= 0.5:
         self.update(x, y, y_hat, n, error)
-        u = np.dot(self.weights, x)
-        epochs += 1
+        u = np.dot(np.transpose(self.weights), x)
       else:
-        break
+        loop = False
 
     return self.weights
 
-  def predict(self, x, activation):
-    u = np.dot(x, self.weights)
-    y = self.activation(activation, u)
+  def predict(self, x):
+    u = np.dot(x, np.transpose(self.weights))
+    y = np.heaviside(u, 0.5)
     if y == 0:
       return 'Iris-setosa'
     if y == 1:
       return 'Iris-versicolor'
 
 perceptron = Perceptron(weights)
-fitted = perceptron.fit(x, y, 'heaviside', 0.1)
-pred = perceptron.predict(np.array([[2, 0.5]]).astype(np.int64), 'heaviside') # should return Iris-setosa
-pred2 = perceptron.predict(np.array([[4, 0.5]]).astype(np.int64), 'heaviside') # should return Iris-versicolor
+fitted = perceptron.fit(x, y, 0.1)
+pred = perceptron.predict([[2, 0.5]]) # should return Iris-setosa
+pred2 = perceptron.predict([[4, 0.5]]) # should return Iris-versicolor
 
-# print(pred)
+print(pred)
 print(pred2)
